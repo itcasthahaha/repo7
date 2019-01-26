127baba5d630b7a46d2858acb9961aad69d773b6
xiaoxiaoxiang <782748443@qq.com>
2019-01-27 00:01:10 +0800

    将本地工程添加到git

---
+++ test-git-project/src/com/itheima/git/TestGit.java
@@ -0,0 +1,12 @@
+package com.itheima.git;
+
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("hello test-git-project");
+        System.out.println("hello test-git-project");
+        System.out.println("hello test-git-project");
+        System.out.println("hello test-git-project");
+        System.out.println("hello test-git-project");
+        System.out.println("hello test-git-project");
+    }
+}
